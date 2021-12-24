d7155b16b3816955b30e23f9ec32bac4f1f9a111
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-25 02:12:02 +0300

    First Commit

---
+++ BookAppMVVMSample/BookAppMVVMSample/BookAppMVVMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppMVVMSampleApp.swift
+//  BookAppMVVMSample
+//
+//  Created by mehmet karanlık on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookAppMVVMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
